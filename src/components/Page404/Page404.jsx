2d244e19b912629adbd497f367aa0806413cb8ed
Justas-Sind <justas.sindar@gmail.com>
2023-02-07 11:42:55 +0200

    page 404 initially complete

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -2,8 +2,11 @@ import styles from "./styles.module.css";
 
 function Page404() {
   return (
-    <>
-    </>
+    <div className={styles.notFoundPage}>
+      <div>
+        <p>Page not found...</p>
+      </div>
+    </div>
   );
 }
 
