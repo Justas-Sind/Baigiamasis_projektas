af767b4c10821c0b7d7d4055afbe7264b8f4ee42
Justas-Sind <justas.sindar@gmail.com>
2023-02-07 11:47:54 +0200

    Added button logic to Home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,13 +1,28 @@
 import styles from "./styles.module.css";
+import UserContext from "../../contexts/UserContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+
+  const navigation = useNavigate();
+
+  const { userloggedIn } = useContext(UserContext);
+
+  function handleButtonClick() {
+    if(!userloggedIn) navigation('/signup')
+    else navigation('/questions')
+  }
+
   return (
     <div className={styles.homePage}>
       <div className={styles.hero}>
         <div className={styles.heroContent}>
           <h1>brain <span>overflow</span></h1>
           <p>A place where all questions are answered</p>
-          <button className={styles.heroButton}>Sign Up</button>
+          <button className={styles.heroButton} onClick={() => handleButtonClick()}>
+            Sign Up
+          </button>
         </div>
       </div>
     </div>
